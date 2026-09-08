c642d87a273fda623a7b5660a37a5ede87d1451a
Braulio Bellodas <nuhkawpipo@gmail.com>
2026-09-08 11:11:53 -0500

    rutas agregadas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,6 +13,13 @@ Route::resource('categorias', CategoriaController::class);
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::resource('usuarios', UserController::class);
+
+    Route::patch('categorias/{categoria}/toggle-status', [CategoriaController::class, 'toggleStatus'])->name('categorias.toggleStatus');
+    Route::resource('categorias', CategoriaController::class);
+
+    Route::patch('productos/{producto}/toggle-status', [ProductoController::class, 'toggleStatus'])->name('productos.toggleStatus');
+    Route::resource('productos', ProductoController::class);
 });
 
 
